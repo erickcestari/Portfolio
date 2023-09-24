5c82a726d1a77a31aebbfee8c188f138c10fb068
Erick Cestari <erickcestari03@gmail.com>
2023-09-24 12:23:48 -0300

    adding load

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,8 +1,11 @@
+import { CircularProgress } from '@mui/material'
 import React from 'react'
 
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div className='flex justify-center items-center h-screen w-screen'>
+      <CircularProgress size={60} sx={{color:"#000"}}/>
+    </div>
   )
 }
 
