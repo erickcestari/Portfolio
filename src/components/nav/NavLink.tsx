c15876a567533c6f82557dd91b14ed9a905952fb
Erick Cestari <erickcestari03@gmail.com>
2023-09-26 22:29:17 -0300

    test

--- src/components/nav/NavLink.tsx
+++ src/components/nav/NavLink.tsx
@@ -8,6 +8,8 @@ interface NavLinkProps {
 const NavLink = (props: NavLinkProps) => {
   const { href, pathName, name } = props
 
+  console.log(pathName, href)
+
   return (
     <Link href={href} className={`hover:underline hover:text-zinc-500 ${pathName === href && "text-blue-700 hover:text-blue-500"}`}>
       {name}
