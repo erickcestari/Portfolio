1d85de719e9e9b918ae543f01bbb1aa6c855f085
Erick Cestari <erickcestari03@gmail.com>
2023-11-12 22:34:58 -0300

    fix: google meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,15 @@ const poppins = Poppins({ weight: ["400", "500", "700"], subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'Erick Cestari Portfolio',
   description: 'Erick Cestari portfolio',
+  openGraph: {
+    type: 'website',
+    url: 'https://erick-cestari-portfolio.vercel.app/',
+    title: 'Erick Cestari Portfolio',
+    description: 'Erick Cestari portfolio',
+  },
+  other: {
+    "google-site-verification": "zOCg1hogkXOKK4nhPfjLxXkHK1_FZxO7YgQg1RH4tF4",
+  }
 }
 
 export default function RootLayout({
@@ -19,21 +28,16 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <>
-      <Head>
-        <meta name="google-site-verification" content="zOCg1hogkXOKK4nhPfjLxXkHK1_FZxO7YgQg1RH4tF4" />
-      </Head>
-      <html lang="en">
-        <body className={`${poppins.className} font-medium bg-gray-200 dark:bg-zinc-900 text-zinc-900 dark:text-gray-200  max-w-5xl p-4 mx-auto`}>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <main>
-              <NavigationBar />
-              {children}
-              <Footer />
-            </main>
-          </ThemeProvider>
-        </body>
-      </html >
-    </>
+    <html lang="en">
+      <body className={`${poppins.className} font-medium bg-gray-200 dark:bg-zinc-900 text-zinc-900 dark:text-gray-200  max-w-5xl p-4 mx-auto`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <main>
+            <NavigationBar />
+            {children}
+            <Footer />
+          </main>
+        </ThemeProvider>
+      </body>
+    </html >
   );
 }
