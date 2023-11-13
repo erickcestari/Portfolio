7fb0de4281c43d566b26e65627a4816af785f1d1
Erick Cestari <erickcestari03@gmail.com>
2023-11-12 22:19:54 -0300

    feat: adding meta tag google-site-verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import NavigationBar from '@/components/nav/NavigationBar'
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from './theme-provider'
-import Footer from '@/components/footer/Footer'
+import Head from 'next/head';
+import NavigationBar from '@/components/nav/NavigationBar';
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from './theme-provider';
+import Footer from '@/components/footer/Footer';
 
 const poppins = Poppins({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
@@ -18,16 +19,21 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${poppins.className} font-medium bg-gray-200 dark:bg-zinc-900 text-zinc-900 dark:text-gray-200  max-w-5xl p-4 mx-auto`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <main>
-            <NavigationBar />
-            {children}
-            <Footer />
-          </main>
-        </ThemeProvider>
-      </body>
-    </html >
-  )
+    <>
+      <Head>
+        <meta name="google-site-verification" content="zOCg1hogkXOKK4nhPfjLxXkHK1_FZxO7YgQg1RH4tF4" />
+      </Head>
+      <html lang="en">
+        <body className={`${poppins.className} font-medium bg-gray-200 dark:bg-zinc-900 text-zinc-900 dark:text-gray-200  max-w-5xl p-4 mx-auto`}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <main>
+              <NavigationBar />
+              {children}
+              <Footer />
+            </main>
+          </ThemeProvider>
+        </body>
+      </html >
+    </>
+  );
 }
