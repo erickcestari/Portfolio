c73dbb58a5efd71c2da4d6bcef0fcfa44ca5efc6
Erick Cestari <erickcestari03@gmail.com>
2023-12-10 11:10:40 -0300

    feat: take the Footer component to the botoom using fixed

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center pb-3 px-3 text-center">
+    <div
+      className="fixed bottom-0 left-0 right-0 flex items-center justify-center pb-3 px-3 text-center"
+    >
       © 2023 Erick Cestari. All rights reserved.
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
