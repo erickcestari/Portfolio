e33abc4195353948637aff77c2a4e71e82947d17
Erick Cestari <erickcestari03@gmail.com>
2023-12-21 14:44:41 -0300

    feat: add footer as relative position

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div
-      className="fixed bottom-0 left-0 right-0 flex items-center justify-center pb-3 px-3 text-center"
+      className="relative flex items-center justify-center pb-3 px-3 text-center"
     >
       © 2023 Erick Cestari. All rights reserved.
     </div>
