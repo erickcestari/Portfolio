3f7156b7606d74b86dcaff6a46f7334a3d67ef40
Erick Cestari <erickcestari03@gmail.com>
2024-01-28 16:38:35 -0300

    feat: first article

--- src/mdx-database/articles/index.ts
+++ src/mdx-database/articles/index.ts
@@ -4,9 +4,9 @@ import { categories } from "../categories";
 export const articles: Article[] = [
   {
     id: 1,
-    title: "0.999... é Realmente Igual a 1?",
+    title: "1 é Igual a 0.99...?",
     brief:
-      "Explorando a fascinante equivalência entre 0.999... e 1 na matemática",
+      "Explorando a fascinante equivalência entre 0.99... e 1 na matemática",
     categories: [
       categories.find(({ title }) => title === "Portuguese")!,
       categories.find(({ title }) => title === "Mathematics")!
@@ -15,8 +15,8 @@ export const articles: Article[] = [
       "Meu primeiro artigo",
     ],
     language: "pt",
-    body: `0.999-igual-a-1`,
-    createdAt: "2023-12-24",
-    updatedAt: "2023-12-24",
+    body: `0.99-igual-a-1`,
+    createdAt: "2024-01-28",
+    updatedAt: "2024-01-28",
   },
 ];
