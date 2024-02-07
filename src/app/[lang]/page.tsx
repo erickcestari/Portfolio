4c38752af9c039ff3e39279c36352bd2442d27a7
Erick 光 <erickcestari03@gmail.com>
2024-02-07 08:05:05 -0300

    feat: revalidate request for each hour

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -9,14 +9,14 @@ const username = "erickcestari"
 
 export default async function Home({ params: { lang } }: LangType) {
   const dic = await getDictionary(lang)
-  const response = await fetch(`https://api.github.com/users/${username}`, githubAuthorization)
+  const response = await fetch(`https://api.github.com/users/${username}`, { ...githubAuthorization, next: { revalidate: 3600 } })
   const dataProfile: GithubProfileType = await response.json()
 
   return (
     <div className='flex flex-col md:flex-row h-full space-x-2 space-y-2 mt-5'>
-      <DisplayProfile profile={dataProfile} dic={dic}/>
+      <DisplayProfile profile={dataProfile} dic={dic} />
       <div className='flex md:h-full border-t-2 border-l-0 border-stone-600 md:border-t-0 md:border-l-2 '>
-        <AboutMe dic={dic}/>
+        <AboutMe dic={dic} />
       </div>
     </div>
   )
