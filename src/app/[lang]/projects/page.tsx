34d949dd8866f5896159b5813c3853fead3d7eca
Erick Cestari <erickcestari03@gmail.com>
2024-11-16 15:45:51 -0300

    feat: display with the most stars

--- src/app/[lang]/projects/page.tsx
+++ src/app/[lang]/projects/page.tsx
@@ -1,9 +1,8 @@
+import { githubAuthorization } from '@/api/github'
 import DisplayGithubRepository from '@/components/projects/DisplayGithubRepository'
 import { getDictionary } from '@/dictionaries/getDictionary'
-import { GithubProfileType } from '@/types/GithubProfileType'
 import { GithubRepository } from '@/types/GithubRepositories'
 import { LangParamsType } from '@/types/params/Language'
-import { githubAuthorization } from '@/api/github'
 
 const Projects = async ({ params }: { params: LangParamsType }) => {
   const { lang } = await params
@@ -12,12 +11,14 @@ const Projects = async ({ params }: { params: LangParamsType }) => {
   const responseRepos = await fetch(url, { ...githubAuthorization, next: { revalidate: 3600 } });
   const githubRepository = await responseRepos.json() as GithubRepository[]
 
+  const sortedRepositories = githubRepository.sort((a, b) => b.stargazers_count - a.stargazers_count);
+
   return (
     <div className='space-y-2 mt-5'>
       <h1 className='text-3xl font-bold'>λ {dic.menu.projects}</h1>
       <div className='space-y-4'>
         <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 min-h-[700px]'>
-          {githubRepository.map((rep) => (
+          {sortedRepositories.map((rep) => (
             <DisplayGithubRepository key={rep.id} githubRepository={rep} dic={dic} />
           ))}
         </div>
